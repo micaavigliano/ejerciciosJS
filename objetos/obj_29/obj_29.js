eebaf5695882b341568f116d03f2f33580c3f228
MicaAvigliano <micaela.avigliano@gmail.com>
2019-03-04 20:33:03 -0300

    salió el push

--- objetos/obj_29/obj_29.js
+++ objetos/obj_29/obj_29.js
@@ -4,40 +4,55 @@ var shoppingCart = {
       name: 'Ada Lovelace'
     },
     products: [],
-    addProduct: function() {
-        shoppingCart.products.push (
-            {
-                id: 1,
-                title: 'Mouse',
-                price: 320,
-                freeShipping: true,
-            },
-            {
-                id: 2,
-                title: 'Gabinete',
-                price: 230,
-                freeShipping: false,
-            },
-            {
-                id: 3,
-                title: 'Teclado',
-                price: 120.50,
-                freeShipping: true,
-            },
-            {
-                id: 4,
-                title: 'Headset',
-                price: 510,
-                freeShipping: false,
-            },
-            {
-                id: 5,
-                title: 'Monitor',
-                price: 125.99,
-                freeShipping: true,
-            },
-)
+    addProduct: function(objetoParaAgregar) {
+        shoppingCart.products.push(objetoParaAgregar)    
     },
+    getTotal: function(sumaObjeto){
+        for (var i = 0; i < shoppingCart.products; i++) {
+            shoppingCart.products['price'] + shoppingCart.products['price']
+        }
+    }
+ }
+
+var mouse = {
+    id: 1,
+    title: 'Mouse',
+    price: 320,
+    freeShipping: true,
+};
+
+var gabinete = {
+    id: 2,
+    title: 'Gabinete',
+    price: 230,
+    freeShipping: false,
+};
+
+var teclado = {
+    id: 3,
+    title: 'Teclado',
+    price: 120.50,
+    freeShipping: true,
 }
-shoppingCart.addProduct();
+
+var headset = {
+    id: 4,
+    title: 'Headset',
+    price: 510,
+    freeShipping: false,
+}
+
+var monitor = {
+    id: 5,
+    title: 'Monitor',
+    price: 125.99,
+    freeShipping: true,
+}
+
+shoppingCart.addProduct(mouse);
+shoppingCart.addProduct(gabinete);
+shoppingCart.addProduct(teclado);
+shoppingCart.addProduct(headset);
+shoppingCart.addProduct(monitor);
 console.log(shoppingCart.products);
+
